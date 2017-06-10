3f981199c1a0996873f3f01b3ccbcfde4cd01b5b
Moon Antonio <pinchodev91@gmail.com>
2017-06-10 20:49:23 +0200

    Refactorizacion de ManualController.cs

--- Roomba 2 Project/Assets/Roomba Proyecto/Scripts/Comun/ManualController.cs
+++ Roomba 2 Project/Assets/Roomba Proyecto/Scripts/Comun/ManualController.cs
@@ -1,31 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// ManualController.cs (22/05/2017)												\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Control manual para el roomba								\\
+// Fecha Mod:		22/05/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
 using UnityEngine;
+#endregion
 
-public class ManualController : MonoBehaviour {
+namespace Antonio.IA
+{
+	/// <summary>
+	/// <para>Control manual para el roomba</para>
+	/// </summary>
+	[AddComponentMenu("UI/Roomba/ManualController")]
+	public class ManualController : MonoBehaviour
+	{
+		#region Actualizadores
+		/// <summary>
+		/// <para>Actualizador de <see cref="ManualController"/>.</para>
+		/// </summary>
+		private void Update()// Actualizador de ManualController
+		{
+			// Gira hacia la izquierda
+			if (Input.GetKeyDown(KeyCode.A))
+			{
+				SendMessage("GirarIzquierda", SendMessageOptions.DontRequireReceiver);
+			}
 
-	// Use this for initialization
-	void Start () {
-		
+			// Gira hacia la derecha
+			if (Input.GetKeyDown(KeyCode.D))
+			{
+				SendMessage("GirarDerecha", SendMessageOptions.DontRequireReceiver);
+			}
+
+			// Avanza hacia delante
+			if (Input.GetKeyDown(KeyCode.W))
+			{
+				SendMessage("Avanzar", SendMessageOptions.DontRequireReceiver);
+			}
+		}
+		#endregion
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            SendMessage("girarIzquierda", SendMessageOptions.DontRequireReceiver);
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            SendMessage("girarDerecha", SendMessageOptions.DontRequireReceiver);
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            SendMessage("avanzar", SendMessageOptions.DontRequireReceiver);
-        }
-
-
-    }
 }
